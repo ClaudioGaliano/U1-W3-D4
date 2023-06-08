8ebe3f6a20aaf2a33bc09939ac3aaf1834d68c2e
Claudio Galiano <134380327+ClaudioGaliano@users.noreply.github.com>
2023-06-09 00:19:26 +0200

    Aggiunta alert window

--- js/tombola.js
+++ js/tombola.js
@@ -5,7 +5,6 @@ const createTombola = function (numbers) {
 
     const tombolaDiv = document.getElementById('tabellaTombola')
     
-
     for (let i = 1; i <= numbers; i++) {
         cellaTombola = document.createElement('div')
         let tombolaNumber = document.createElement('h3')
@@ -16,21 +15,22 @@ const createTombola = function (numbers) {
     }
     
 }
+
 createTombola(76)
 
-const EstrazioneNumero = function (e) {
+const EstrazioneNumero = (e) => {
     let n = Math.floor(Math.random() * 77);
     if (!numeriEstratti.includes(n) && n !== 0) {
-        console.log(n);
         let numberOnTable = document.querySelectorAll('.tombolaNumberStyle h3')[n-1]
         console.log(numberOnTable);
         numberOnTable.classList.add('estratto')
         numeriEstratti.push(n)
         console.log(numeriEstratti);
         
+    } else if(numeriEstratti.length === 76) {
+        alert("Sono usciti tutti i numeri")
     } else EstrazioneNumero()
 }
-
 
 const estrazioneButton = document.querySelector('button')
 estrazioneButton.addEventListener("click", EstrazioneNumero)
